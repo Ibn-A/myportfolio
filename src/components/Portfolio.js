ef1f96ac7297fc9dd3edfafa521ef72d0d1b7d38
Ibn-A <ibn_ali@yahoo.com>
2020-08-17 23:25:30 +0200

    Deplacement de la navbar hors de la balise Box

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -33,131 +33,135 @@ const Portfolio = () => {
     const classes = useStyles();
 
     return (
-        <Box component="div" className={classes.mainContainer}>
+        <>
             <Navbar />
-            <Grid container spacing={3} justify="center">
-                {/* Project1 */}
-                <Grid item xs={12} sm={8} md={6}>
-                    <Card className={classes.cardContainer}>
-                        <CardActionArea>
-                            <CardMedia 
-                                component="img"
-                                alt="Project 1"
-                                height="140"
-                                image={project1}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5">
-                                    Project 1
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
-                                </Typography>
-                            </CardContent>
+            <Box component="div" className={classes.mainContainer}>
+                <Grid container spacing={3} justify="center">
+                    {/* Project1 */}
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardMedia 
+                                    component="img"
+                                    alt="Project 1"
+                                    height="140"
+                                    image={project1}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5">
+                                        Project 1
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
+                                    </Typography>
+                                </CardContent>
 
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Share
-                            </Button>
-                            <Button size="small" color="primary">
-                                Live Demo
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                {/* Project2 */}
-                <Grid item xs={12} sm={8} md={6}>
-                    <Card className={classes.cardContainer}>
-                        <CardActionArea>
-                            <CardMedia 
-                                component="img"
-                                alt="Project 2"
-                                height="140"
-                                image={project2}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5">
-                                    trocServices App
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Share
-                            </Button>
-                            <Button size="small" color="primary">
-                                Live Demo
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                {/* Project3 */}
-                <Grid item xs={12} sm={8} md={6}>
-                    <Card className={classes.cardContainer}>
-                        <CardActionArea>
-                            <CardMedia 
-                                component="img"
-                                alt="Project 3"
-                                height="140"
-                                image={project3}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5">
-                                    My Blog
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
-                                </Typography>
-                            </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Share
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Live Demo
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    {/* Project2 */}
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardMedia 
+                                    component="img"
+                                    alt="Project 2"
+                                    height="140"
+                                    image={project2}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5">
+                                        trocServices App
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Share
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Live Demo
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    {/* Project3 */}
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardMedia 
+                                    component="img"
+                                    alt="Project 3"
+                                    height="140"
+                                    image={project3}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5">
+                                        My Blog
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
+                                    </Typography>
+                                </CardContent>
 
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Share
-                            </Button>
-                            <Button size="small" color="primary">
-                                Live Demo
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                {/* Project4 */}
-                <Grid item xs={12} sm={8} md={6}>
-                    <Card className={classes.cardContainer}>
-                        <CardActionArea>
-                            <CardMedia 
-                                component="img"
-                                alt="Project 4"
-                                height="140"
-                                image={project4}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5">
-                                    Elan Formation webApp
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
-                                </Typography>
-                            </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Share
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Live Demo
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    {/* Project4 */}
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardMedia 
+                                    component="img"
+                                    alt="Project 4"
+                                    height="140"
+                                    image={project4}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5">
+                                        Elan Formation webApp
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
+                                    </Typography>
+                                </CardContent>
 
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Share
-                            </Button>
-                            <Button size="small" color="primary">
-                                Live Demo
-                            </Button>
-                        </CardActions>
-                    </Card>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Share
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Live Demo
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
-            
-        </Box>
+                
+            </Box>
+
+        </>
+        
     )
 }
 
