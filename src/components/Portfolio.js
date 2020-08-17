aa6e63578ed4c83113c64b256a53131242c3a182
Ibn-A <ibn_ali@yahoo.com>
2020-08-17 22:36:52 +0200

    Creation de 3 autres fiches projets dans le portfolio component.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,7 +12,10 @@ import {
     Button,
     Typography,
 } from '@material-ui/core';
-import project1 from '../images/reactjs.png'
+import project1 from '../images/reactjs.png';
+import project2 from '../images/laravel.jpg';
+import project3 from '../images/php.png';
+import project4 from '../images/symfony.png';
 
 const useStyles = makeStyles({
     mainContainer: {
@@ -46,6 +49,95 @@ const Portfolio = () => {
                             <CardContent>
                                 <Typography gutterBottom variant="h5">
                                     Project 1
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
+                                </Typography>
+                            </CardContent>
+
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Live Demo
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                {/* Project2 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia 
+                                component="img"
+                                alt="Project 2"
+                                height="140"
+                                image={project2}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    trocServices App
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Live Demo
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                {/* Project3 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia 
+                                component="img"
+                                alt="Project 3"
+                                height="140"
+                                image={project3}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    My Blog
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
+                                </Typography>
+                            </CardContent>
+
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Live Demo
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                {/* Project4 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia 
+                                component="img"
+                                alt="Project 4"
+                                height="140"
+                                image={project4}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Elan Formation webApp
                                 </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     LoremVoluptate ut et minim deserunt voluptate fugiat ullamco. Labore irure in ipsum adipisicing commodo elit est officia. Id enim nostrud labore voluptate dolore esse ullamco qui. Magna occaecat aliqua est eiusmod tempor consectetur sint nisi reprehenderit ullamco ea quis pariatur duis. Aute Lorem ipsum ea dolor dolore quis nulla mollit do deserunt officia magna do magna. 
